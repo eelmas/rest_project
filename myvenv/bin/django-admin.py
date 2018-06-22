164186209f993a380e1e203f83163d04116240c7
elmas <eedagli@gmail.com>
2018-06-22 12:01:31 +0300

    proje dosyalari eklendi

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Elmas/Documents/django_rest_projects/rest_project/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
